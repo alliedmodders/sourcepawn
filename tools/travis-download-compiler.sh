fbc2b143ff88f36c4c7a683e2fc979bf53343e64
Asher Baker <asherkin@limetech.io>
2018-02-07 12:19:12 +0000

    Switch to Urho3D's prebuilt emsdk for Travis

--- tools/travis-download-compiler.sh
+++ tools/travis-download-compiler.sh
@@ -3,15 +3,14 @@
 set -e
 
 if [ "$AM_CC" == "emcc" ]; then
-  wget -nc $EMSCRIPTEN_SDK_URI -O $HOME/emsdk-portable.tar.gz
-  tar -xf $HOME/emsdk-portable.tar.gz -C $HOME
-  pushd $HOME/emsdk-portable
-  ./emsdk update
-  ./emsdk install latest
-  ./emsdk activate latest
-  popd
+  git clone --depth 1 --branch $EMSCRIPTEN_SDK_BRANCH $EMSCRIPTEN_SDK_URI $HOME/emscripten-sdk
+  $HOME/emscripten-sdk/emsdk activate --build=Release sdk-$EMSCRIPTEN_SDK_BRANCH-64bit
 
-  source $HOME/emsdk-portable/emsdk_env.sh
+  source $HOME/emscripten-sdk/emsdk_env.sh
+
+  for compiler in $EMSCRIPTEN/{emcc,em++}; do
+    touch -d "2017-01-01 00:00:00 +0800" $compiler
+  done
 fi
 
 set "$state"
