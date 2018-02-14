e983f6face3bee22d43f48e649b1440e78c0bfa0
Kruzya <webmaster@kruzefag.ru>
2018-02-14 18:25:12 +0400

    Added __line const

--- compiler/sc1.cpp
+++ compiler/sc1.cpp
@@ -1159,6 +1159,7 @@ static void setconstants(void)
   add_constant("ucharmax",(1 << (sizeof(cell)-1)*8)-1,sGLOBAL,0);
 
   add_constant("__Pawn",VERSION_INT,sGLOBAL,0);
+  add_constant("__line",0,sGLOBAL,0);
 
   debug=0;
   if ((sc_debug & (sCHKBOUNDS | sSYMBOLIC))==(sCHKBOUNDS | sSYMBOLIC))
