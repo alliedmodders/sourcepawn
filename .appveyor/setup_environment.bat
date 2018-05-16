45b29a57196866b0a4771c09e21759b235951096
Fyren <fyrenmoo@gmail.com>
2018-05-16 09:38:34 +0000

    Fix AppVeyor not actually building or running tests.

--- .appveyor/setup_environment.bat
+++ .appveyor/setup_environment.bat
@@ -1,6 +1,6 @@
+@echo on
+git submodule update --init
 git clone https://github.com/alliedmodders/ambuild ambuild
 cd ambuild
 c:\python27\python.exe setup.py install
-chdir /D c:\projects\
-git clone https://github.com/alliedmodders/amtl amtl
 chdir /D "%APPVEYOR_BUILD_FOLDER%"
