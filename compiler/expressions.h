1cd9658b2f5b983b4aa7c83b261a27869c977205
David Anderson <dvander@alliedmods.net>
2019-11-24 21:15:26 -0800

    Remove the old expression parser.

--- compiler/expressions.h
+++ compiler/expressions.h
@@ -50,49 +50,6 @@ class ExpressionParser
     static int list12[];
 };
 
-class SC3ExpressionParser : public ExpressionParser
-{
-  public:
-    int evaluate(value* lval) {
-        return hier14(lval);
-    }
-
-  private:
-    typedef int (SC3ExpressionParser::*HierFn)(value*);
-
-    int hier14(value* lval);
-    int hier13(value* lval);
-    int hier12(value* lval);
-    int hier11(value* lval);
-    int hier10(value* lval);
-    int hier9(value* lval);
-    int hier8(value* lval);
-    int hier7(value* lval);
-    int hier6(value* lval);
-    int hier5(value* lval);
-    int hier4(value* lval);
-    int hier3(value* lval);
-    int hier2(value* lval);
-    int hier1(value* lval1);
-    int plnge(int* opstr, int opoff, HierFn hier, value* lval, const char* forcetag,
-              int chkbitwise);
-    int plnge1(HierFn hier, value* lval);
-    void plnge2(void (*oper)(void), HierFn hier, value* lval1, value* lval2);
-    int plnge_rel(int* opstr, int opoff, HierFn hier, value* lval);
-    void callfunction(symbol* sym, const svalue* implicitthis, value* lval_result,
-                      int matchparanthesis);
-    int primary(value* lval);
-    int skim(int* opstr, void (*testfunc)(int), int dropval, int endval, HierFn hier, value* lval);
-    int parse_view_as(value* lval);
-    cell parse_defined();
-    cell parse_sizeof();
-    cell sizeof_impl();
-
-  private:
-    // Count of bitwise operators in an expression.
-    int bitwise_opercount_ = 0;
-};
-
 #define MATCHTAG_COERCE 0x1      // allow coercion
 #define MATCHTAG_SILENT 0x2      // silence the error(213) warning
 #define MATCHTAG_COMMUTATIVE 0x4 // order does not matter
