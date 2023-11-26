6ced9953a5d6537199069eb6a77a910a6db64760
David Anderson <dvander@alliedmods.net>
2023-11-26 08:56:58 -0500

    Remove unused iSCOPE.

--- compiler/types.h
+++ compiler/types.h
@@ -1,28 +1,23 @@
 /* vim: set sts=4 ts=8 sw=4 tw=99 et: */
-/*  Pawn compiler
- *
- *  Function and variable definition and declaration, statement parser.
- *
- *  Copyright (c) ITB CompuPhase, 1997-2006
- *
- *  This software is provided "as-is", without any express or implied warranty.
- *  In no event will the authors be held liable for any damages arising from
- *  the use of this software.
- *
- *  Permission is granted to anyone to use this software for any purpose,
- *  including commercial applications, and to alter it and redistribute it
- *  freely, subject to the following restrictions:
- *
- *  1.  The origin of this software must not be misrepresented; you must not
- *      claim that you wrote the original software. If you use this software in
- *      a product, an acknowledgment in the product documentation would be
- *      appreciated but is not required.
- *  2.  Altered source versions must be plainly marked as such, and must not be
- *      misrepresented as being the original software.
- *  3.  This notice may not be removed or altered from any source distribution.
- *
- *  Version: $Id$
- */
+//
+//  Copyright (c) ITB CompuPhase, 1997-2006
+//  Copyright (c) AlliedModders LLC, 2023
+//
+//  This software is provided "as-is", without any express or implied warranty.
+//  In no event will the authors be held liable for any damages arising from
+//  the use of this software.
+//
+//  Permission is granted to anyone to use this software for any purpose,
+//  including commercial applications, and to alter it and redistribute it
+//  freely, subject to the following restrictions:
+//
+//  1.  The origin of this software must not be misrepresented; you must not
+//      claim that you wrote the original software. If you use this software in
+//      a product, an acknowledgment in the product documentation would be
+//      appreciated but is not required.
+//  2.  Altered source versions must be plainly marked as such, and must not be
+//      misrepresented as being the original software.
+//  3.  This notice may not be removed or altered from any source distribution.
 #pragma once
 
 #include <memory>
@@ -61,7 +56,6 @@ enum IdentifierKind {
     iACCESSOR = 13,     /* property accessor via a methodmap_method_t */
     iMETHODMAP = 14,    /* symbol defining a methodmap */
     iENUMSTRUCT = 15,   /* symbol defining an enumstruct */
-    iSCOPE = 16,        /* local scope chain */
 };
 
 enum class TypeKind : uint8_t {
