59f522f80a3cff123b190a87d1885f56d8b75624
David Anderson <dvander@alliedmods.net>
2026-08-30 19:59:28 -0700

    Fix int64 compilation.

--- compiler/code-generator.cpp
+++ compiler/code-generator.cpp
@@ -1968,13 +1968,17 @@ void CodeGenerator::EmitSimpleCastExpr(SimpleCastExpr* expr) {
     }
 }
 
+static inline bool CoercesToInt64(Type* type) {
+    return type->isInt() || type->isAny();
+}
+
 void CodeGenerator::EmitCastExpr(CastExpr* expr) {
     auto from = expr->expr();
     EmitExpr(from);
 
-    if (expr->val().type()->isInt() && from->val().type()->isInt64()) {
+    if (CoercesToInt64(expr->val().type()) && from->val().type()->isInt64()) {
         __ emit(OP_TRUNCATE_I64);
-    } else if (expr->val().type()->isInt64() && from->val().type()->isInt()) {
+    } else if (expr->val().type()->isInt64() && CoercesToInt64(from->val().type())) {
         __ emit(OP_CVT_I64);
     }
 }
