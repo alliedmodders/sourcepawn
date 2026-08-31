da3cb70512a635737c5e838c67a22493c6bd974e
David Anderson <dvander@alliedmods.net>
2026-08-30 19:59:30 -0700

    Fix crash when using int64 in the preprocessor.

--- compiler/parser.cpp
+++ compiler/parser.cpp
@@ -341,6 +341,11 @@ bool Parser::PreprocExpr(cell* val, Type** type) {
     if (!expr->Bind(sc) || !sema.CheckExpr(expr))
         return false;
 
+    if (!expr->val().type()->isInt() && !expr->val().type()->isBool()) {
+        report(135);
+        return false;
+    }
+
     // The preprocessor uses a bespoke constant evaluator, and I don't want to
     // touch the preprocessor, so it gets to keep this.
     return expr->EvalConst(val, type);
